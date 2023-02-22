4a44879d215743a444939bf559651fb17d48e4ad
nthokiesther09 <nthokiesther09@gmail.com>
2023-02-22 10:17:05 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) 
+VALUES("Mr.Chocolate", 3, "F", "brown", "supiene", true), 
+    ("Rowdy", 4, "F", "grey", "sanguine", true), 
+    ("Tabitha", 6, "F", "white_dotted", "melancholy", false),
+    ("Sergeant Brown", 4, "F", "grey", "sanguine", true),
+    ("Melissa", 5, "M", "dark_brown", "phlegmatic", false),
+    ("Grinch", 4, "F", "grey", "supine", true),
+    ("Wendy", 5, "M", "black", "sanguine", true),
+    (NULL, 7, "F", "beige", "choleric", true); 
